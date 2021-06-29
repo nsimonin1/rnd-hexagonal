7c3a0fe85d0be709afa5d269eddda9169bc2ffcf
nsimonin1 <nsimonin1@gmail.com>
2021-06-29 10:12:41 +0200

    init maven project

---
+++ backend-spring-service/src/main/java/org/afrinnov/rnd/hex/BackendSpringServiceApplication.java
@@ -0,0 +1,13 @@
+package org.afrinnov.rnd.hex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSpringServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSpringServiceApplication.class, args);
+	}
+
+}
---
+++ backend-spring-service/src/test/java/org/afrinnov/rnd/hex/BackendSpringServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.afrinnov.rnd.hex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
